7c75288a7df0bd7120779d6a767750f16b2a510b
Heshan Padmasiri <hpheshan@gmail.com>
2022-12-23 16:42:20 +0530

    Initial code setup

---
+++ src/client.rs
@@ -0,0 +1,21 @@
+use oxygen::{oxygen_client::OxygenClient, Req};
+
+pub mod oxygen {
+    tonic::include_proto!("oxygen");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = OxygenClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(
+        Req {
+            msg: "test message".to_owned(),
+            id: 0
+        }
+    );
+
+    let response = client.hello(request).await?;
+    println!("RESPONSE={:?}", response);
+    Ok(())
+}
---
+++ src/server.rs
@@ -0,0 +1,38 @@
+use oxygen::{
+    oxygen_server::{Oxygen, OxygenServer},
+    Req, Res,
+};
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod oxygen {
+    tonic::include_proto!("oxygen");
+}
+
+#[derive(Default)]
+pub struct OxygenService {}
+
+#[tonic::async_trait]
+impl Oxygen for OxygenService {
+    async fn hello(&self, request: Request<Req>) -> Result<Response<Res>, Status> {
+        println!("Got request: {:?}", request);
+        let req = request.into_inner();
+
+        let reply = Res {
+            msg: format!("Recieved : {}", req.msg).into(),
+            successful: true,
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let oxygen_service = OxygenService::default();
+    Server::builder()
+        .add_service(OxygenServer::new(oxygen_service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
